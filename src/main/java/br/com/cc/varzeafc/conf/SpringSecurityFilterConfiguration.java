53ccd46ef3798fd422c527ee906d925702090152
Thiago Dias <thiago-dias@outlook.com>
2017-03-21 21:47:57 -0300

    Commit Inicial

---
+++ src/main/java/br/com/cc/varzeafc/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.cc.varzeafc.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer
+{
+
+}
